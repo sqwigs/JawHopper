2b521657ecf7c0587f3a7b91cc819b2d9f0b9501
Joshua Beitler <joshbeitler@gmail.com>
2018-03-09 21:10:26 -0700

    Items delete themselves

--- JawHopperGame/Assets/Scripts/SpeedItem.cs
+++ JawHopperGame/Assets/Scripts/SpeedItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedItem : MonoBehaviour, BasicItem  {
+public class SpeedItem : BasicItem  {
     public float SpeedModifier
     {
         get
