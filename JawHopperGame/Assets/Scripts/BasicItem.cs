58092aad229fa6ac64fcb2045437a7dc02db3368
JARED SCOTT SAGER <u1136374@ad.utah.edu>
2018-03-09 22:01:44 -0700

    Fix conflict I hope please?

--- JawHopperGame/Assets/Scripts/BasicItem.cs
+++ JawHopperGame/Assets/Scripts/BasicItem.cs
@@ -26,12 +26,22 @@ public class BasicItem : MonoBehaviour
     {
         if (collider.gameObject.tag == "Player" && pickedUp == false)
         {
+<<<<<<< 01b0f20275bfcf6982742a2f90c00f6285219b04
             var player = collider.gameObject.GetComponent<PlayerController>();
 
             player.jumpModifier = this.JumpModifier;
             player.speedModifier = this.SpeedModifier;
 
+=======
+>>>>>>> Reconfigured Item pickup code
             Destroy(this.gameObject);
+
+            pickedUp = true;
+
+            var playerController = collider.gameObject.GetComponent<PlayerController>();
+            playerController.currentItem = Instantiate(this);
+
+            
         }
     }
 
