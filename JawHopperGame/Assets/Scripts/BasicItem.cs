cd68931504ec413e832ab319202259160453d06c
Joshua Beitler <joshbeitler@gmail.com>
2018-03-09 20:39:38 -0700

    Add items

---
+++ JawHopperGame/Assets/Scripts/BasicItem.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface BasicItem {
+    float SpeedModifier { get; }
+    float JumpModifier { get; }
+}
