34923ecc63c750d9914c2ec4e9dda9fc6fd2fb84
JARED SCOTT SAGER <u1136374@ad.utah.edu>
2018-03-09 19:28:44 -0700

    added test scene and base item script

---
+++ JawHopperGame/Assets/Scripts/BasicItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicItem : MonoBehaviour {
+
+    public float jumpModifier;
+    public float speedModifier;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
