22c50b56efbcdd86257fd0127185ad6143b107fe
Michael Gardone <u1000771@ad.utah.edu>
2018-03-09 20:29:51 -0700

    Oneway platforms are a thing

--- JawHopperGame/Assets/Scripts/PlatformCollider.cs
+++ JawHopperGame/Assets/Scripts/PlatformCollider.cs
@@ -2,75 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[RequireComponent(typeof(MeshCollider))]
 public class PlatformCollider : MonoBehaviour {
+    MeshCollider floor;
 
-
-    public float maxAngle = 45.0f;
+    // Use this for initialization
     void Start()
     {
-        float cos = Mathf.Cos(maxAngle);
-        MeshFilter MF = GetComponent<MeshFilter>();
-        MeshCollider MC = GetComponent<MeshCollider>();
-        if (MF == null || MC == null || MF.sharedMesh == null)
-        {
-            Debug.LogError("PlatformCollision needs a MeshFilter and a MeshCollider");
-            return;
-        }
-        Mesh M = new Mesh();
-        Vector3[] verts = MF.sharedMesh.vertices;
-        List<int> triangles = new List<int>(MF.sharedMesh.triangles);
-        for (int i = triangles.Count - 1; i >= 0; i -= 3)
-        {
-            Vector3 P1 = transform.TransformPoint(verts[triangles[i - 2]]);
-            Vector3 P2 = transform.TransformPoint(verts[triangles[i - 1]]);
-            Vector3 P3 = transform.TransformPoint(verts[triangles[i]]);
-            Vector3 faceNormal = Vector3.Cross(P3 - P2, P1 - P2).normalized;
-            if (Vector3.Dot(faceNormal, Vector3.up) <= cos)
-            {
-                triangles.RemoveAt(i);
-                triangles.RemoveAt(i - 1);
-                triangles.RemoveAt(i - 2);
-            }
-        }
-        M.vertices = verts;
-        M.triangles = triangles.ToArray();
-        MC.sharedMesh = M;
+        floor = GetComponentInChildren<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 
 
-    /*BoxCollider topCol, botCol;
-
-    // Use this for initialization
-	void Start () {
-        topCol = GetComponentInChildren<BoxCollider>();
-        botCol = GetComponentInChildren<BoxCollider>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject.name);
-        Debug.Log(name);
-        
-        if (other.gameObject.GetComponent<BoxCollider>() == botCol)
-        {
-            topCol.enabled = false;
-        }
+        floor.enabled = false;
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<BoxCollider>() == botCol)
-        {
-            topCol.enabled = true;
-        }
-    }*/
+        floor.enabled = true;
+    }
 
 }
