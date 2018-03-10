231f8b00d32611e46f174d54134dad56b6145e63
Kindra Lingenfelter <scrunnchy@gmail.com>
2018-03-09 20:42:43 -0700

    added level loading

--- JawHopperGame/Assets/Scripts/LevelSelect.cs
+++ JawHopperGame/Assets/Scripts/LevelSelect.cs
@@ -8,8 +8,10 @@ using UnityEngine.Events;
 
 public class LevelSelect : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private UnityAction load;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -23,13 +25,15 @@ public class LevelSelect : MonoBehaviour {
         level1.onClick.AddListener(loadLevel(level1.name));
     }
 
+    private UnityAction loadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+        return load;
+    }
+
     private void goBack()
     {
         //SceneManager.LoadScene();
     }
 
-    private UnityAction loadLevel(string name)
-    {
-        throw new NotImplementedException();
-    }
 }
