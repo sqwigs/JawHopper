e7fa017a90d13e9dfe845e200ff530c90ebe6973
singh1rocks <shakti.das@aol.com>
2018-03-09 19:01:52 -0700

    a

--- JawHopperGameJam/Assets/Scripts/CameraView.cs
+++ JawHopperGameJam/Assets/Scripts/CameraView.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+    public GameObject player;
+
+    private Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
-		
+        offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
+        transform.position = player.transform.position = offset;
 		
 	}
 }
