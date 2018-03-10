3d89e3c7de42c16240400bded569674305a0e9a6
Kindra Lingenfelter <scrunnchy@gmail.com>
2018-03-09 20:00:43 -0700

    MainMenu is finished... probably

---
+++ JawHopperGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Button startButton = GameObject.Find("startButton").GetComponent<Button>();
+        Button endButton = GameObject.Find("endButton").GetComponent<Button>();
+
+        startButton.onClick.AddListener(startGame);
+        endButton.onClick.AddListener(endGame);
+
+    }
+
+    private void endGame()
+    {
+        //Button endButton = GameObject.Find("endButton").GetComponent<Button>();
+        //ColorBlock theColor = endButton.colors;
+        //theColor.highlightedColor = Color.blue;
+        //theColor.normalColor = Color.cyan;
+        //theColor.pressedColor = Color.red;
+
+        //endButton.colors = theColor;
+
+        Application.Quit();
+    }
+
+    private void startGame()
+    {
+        //Button startButton = GameObject.Find("startButton").GetComponent<Button>();
+        //ColorBlock theColor = startButton.colors;
+        //theColor.highlightedColor = Color.blue;
+        //theColor.normalColor = Color.cyan;
+        //theColor.pressedColor = Color.red;
+
+        //startButton.colors = theColor;
+
+        SceneManager.LoadScene("Level1");
+    }
+}
