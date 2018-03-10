b3be11e804039405434f580b2250148389cb9dbc
Cody Spainhower <u0940176@ad.utah.edu>
2018-03-09 19:16:42 -0700

    player movement proto

--- JawHopperGameJam/Assets/PlayerController.cs
+++ JawHopperGameJam/Assets/PlayerController.cs
@@ -4,13 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+    Rigidbody body;
+    public Vector3 velocity = new Vector3();
+    public Transform raycastTarget;
+
+    public float jumpSpeed = 10;
+    public float sideSpeed = 5;
+
+    float dir = 1;
+
+
 	void Start () {
-		
+        body = GetComponent<Rigidbody>();
+        body.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        bool grounded = false;
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.55f))
+        {
+            if (hit.collider.gameObject.tag == "Map")
+            {
+                grounded = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();          
+        }
+        else if (grounded)
+        {
+            Walk();
+        }
 		
 	}
+
+    void Jump()
+    {
+        dir = -dir;
+        velocity = new Vector3(sideSpeed * dir, jumpSpeed, 0);
+        body.velocity = velocity;
+    }
+
+    void Walk()
+    {
+        velocity = new Vector3(sideSpeed * dir, 0, 0);
+        body.velocity = velocity;
+    }
 }
